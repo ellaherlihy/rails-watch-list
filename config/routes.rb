98402d4b7d357f412e8a987f904a48fc1c4b860e
Ella Herlihy <ellaherlihy@gmail.com>
2022-05-28 08:10:28 +0100

    hosted on heroku and styling updates

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+    root "lists#index"
 
     resources :lists, only: %i[show new create index] do
     resources :bookmarks, only: %i[new create]
